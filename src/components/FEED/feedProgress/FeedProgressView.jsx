e0f2ad44863794adda21ceccd50358e04bb2cdf0
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-01-26 11:34:29 +0100

    Feed progress style working

--- src/components/FEED/feedProgress/FeedProgressView.jsx
+++ src/components/FEED/feedProgress/FeedProgressView.jsx
@@ -15,7 +15,7 @@ import {
 } from "recharts";
 
 import { api } from "../../../helpers/api";
-import Button1 from "../../general/Button1";
+import Button1 from "../../general/Button2";
 import { prepareRows } from "./feedProgressHelpers";
 
 export default function FeedProgressView() {
@@ -37,50 +37,57 @@ export default function FeedProgressView() {
         <h3 className="centerTitle">Feed Progress</h3>
         <Button1
           text="Edit Forecast"
-          width="150px"
-          border="1px solid black"
-          bgColor="#0070ED"
-          bgHover="#3e96fa"
+          width="140px"
+          bgColor="transparent"
+          border="none"
           color="white"
+          fontWeight="600"
+          fontSize="14px"
+          textMargin="0 0 0 6px"
           margin="auto"
+          hoverShadow="inset 5px 5px 10px #0061ce, inset -5px -5px 10px #007fff"
+          // img
+          alt="Edit Forecast"
+          src={"https://img.icons8.com/color/search"}
+          imgFilter="invert(100%) brightness(200%)"
           onClick={() => navigate("/feed/progress/edit_forecast")}
         />
       </div>
-      <ResponsiveContainer width="92%" height="70%" className="chart">
-        <LineChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 35,
-            right: 0,
-            left: 20,
-            bottom: -15,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis
-            label={{
-              value: "Weight",
-              position: "insideLeft",
-              angle: -90,
-              dy: 30,
+      <div className="chartWrapper flexCenter">
+        <ResponsiveContainer width="80%" height="90%" className="chart">
+          <LineChart
+            data={data}
+            margin={{
+              top: 35,
+              right: 30,
+              left: 20,
+              bottom: -15,
             }}
-          />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="current_weight"
-            stroke="blue"
-            activeDot={{ r: 8 }}
-          />
-          <Line type="monotone" dataKey="max_weight" stroke="red" />
-          <Line type="monotone" dataKey="estimated" stroke="green" />
-          <Line type="monotone" dataKey="forecast" stroke="brown" />
-        </LineChart>
-      </ResponsiveContainer>
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis
+              label={{
+                value: "Weight",
+                position: "insideLeft",
+                angle: -90,
+                dy: 30,
+              }}
+            />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="current_weight"
+              stroke="blue"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="max_weight" stroke="red" />
+            <Line type="monotone" dataKey="estimated" stroke="green" />
+            <Line type="monotone" dataKey="forecast" stroke="brown" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
@@ -91,12 +98,11 @@ const headStyle = {
   height: "50px",
   width: "100%",
   backgroundColor: "#338DF1",
-  borderRadius: "0 20px 0 0",
   h3: {
     fontSize: "1.2rem",
     whiteSpace: "nowrap",
     color: "white",
-    marginLeft: "41%"
+    marginLeft: "50%",
   },
   ".centerTitle": {
     display: "flex",
@@ -106,15 +112,18 @@ const headStyle = {
 
 const forecastStyles = {
   width: "100%",
-  height: "82.5%",
-  h3: {
-    fontSize: "20px",
-  },
+  height: "60vh",
   button: {
     alignSelf: "flex-end",
     marginRight: "10%",
   },
   ".chart": {
     alignSelf: "flex-start",
+  },
+  ".chartWrapper": {
+    width: "100%",
+    height: "calc(60vh - 50px)",
+    border: "solid #D2D2D2",
+    borderWidth: "0 1px 1px 1px"
   },
 };
