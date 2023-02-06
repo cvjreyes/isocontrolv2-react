6b94625bcdaeb958ef8348995ddee0a7753867ad
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-02-06 11:30:24 +0100

    Quitando log

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { URL } from "./config";
 
 export const api = async (action, url, body) => {
-  console.log("Body: ", body);
   try {
     const res = await axios[action](`${URL}${url}`, body);
     return res.data;
