f6ed05eb8229f6b69bbb617f9fa727ead9181972
Toni <webdevtoni@gmail.com>
2023-02-15 10:53:03 +0100

    working on notifications

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import { useEffect, useState } from "react";
+
+import { api } from "../helpers/api";
 
 export default function Notifications() {
-  return <div>Notifications</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const getNotifications = async () => {
+      const res = await api("get", `/notifications/get_some/${count}`);
+    };
+    getNotifications();
+  }, [count]);
+
+  return (
+    <div css={notificationsStyle}>
+      {/* {notificationsStyle.map(x => )} */}
+      <button className="removeStyle">See more</button>
+    </div>
+  );
 }
+
+const notificationsStyle = {};
