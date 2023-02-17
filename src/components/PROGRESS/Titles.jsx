6de90226645f3449725e4e462e021b75c26467bb
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-02-17 09:44:39 +0100

    Titulos del Progress mejorados

--- src/components/PROGRESS/Titles.jsx
+++ src/components/PROGRESS/Titles.jsx
@@ -3,27 +3,35 @@
 import { jsx } from "@emotion/react";
 import { NavLink } from "react-router-dom";
 
+import Button1 from "../general/Button1";
+
 export default function Titles() {
-  const links = [{ label: "FEED" }, { label: "IFD" }, { label: "IFC" }];
+  const links = [{ label: "FEED" }, { label: "IFD" }, /*{ label: "IFC" }*/];
 
   const titlesStyle = {
     display: "grid",
     gridTemplateColumns: `repeat(${links.length}, 1fr)`,
-    textAlign: "center",
     width: "fit-content",
     margin: "3% auto 0",
-    ".link": {
-      margin:"0 2rem"
-    }
-
+    ".boxlink": {
+      margin: "0 3rem",
+    },
   };
 
   return (
     <div css={titlesStyle}>
       {links.map((x, i) => {
         return (
-          <div key={i} className="link" >
-            <NavLink to={`/progress/${x.label}`}>{x.label}</NavLink>
+          <div key={i} className="boxlink">
+            <NavLink to={`/progress/${x.label}`}>
+              <Button1
+                text={x.label}
+                bgColor="#338DF1"
+                color="white"
+                fontWeight="bold"
+                bgHover="#66A9F4"
+              />
+            </NavLink>
           </div>
         );
       })}
