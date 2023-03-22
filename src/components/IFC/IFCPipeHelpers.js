8f6a105a939b6c951300881b54a260a825e124e5
Toni <webdevtoni@gmail.com>
2023-03-22 15:47:20 +0100

    next step working among other stuff

---
+++ src/components/IFC/IFCPipeHelpers.js
@@ -0,0 +1,34 @@
+const progressNumbers = {
+  TL1: {
+    design: 0,
+    materials: 85,
+    issuer: 90,
+    toissue: 95,
+    issued: 100,
+  },
+  TL2: {
+    design: 0,
+    supports: 80,
+    materials: 85,
+    issuer: 90,
+    toissue: 95,
+    issued: 100,
+  },
+  TL3: {
+    design: 0,
+    stress: 60,
+    supports: 80,
+    materials: 85,
+    issuer: 90,
+    toissue: 95,
+    issued: 100,
+  },
+};
+
+export const calculateNextStep = (status, type) => {
+  const list = Object.keys(progressNumbers[type]);
+  const tempStatus = status.replace("-", "").replace("*", "").toLowerCase();
+  const idx = list.findIndex((item) => tempStatus == item);
+  const nextStep = list[idx + 1];
+  return nextStep;
+};
